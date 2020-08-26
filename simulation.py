9f97daa2701c111523528af64ef56cdd1bda718d
Steven Verwerft <33197418+StevenVerwerft@users.noreply.github.com>
2020-08-26 16:31:36 +0200

    removed old functions

--- simulation.py
+++ simulation.py
@@ -6,7 +6,6 @@ import json
 
 print("Start simulation")
 GENERATE_INPUT_JSON = False  # json hoeft niet steeds gegenereerd te worden 
-GENERATE_OUTPUT_JSON = False
 
 # preprocessing van excel file
 print("Preprocessing data excel...")
@@ -37,21 +36,3 @@ boston(instance)
 # POST PROCESSING
 print("Generating report...")
 generate_report(instance, excel_df)
-
-if GENERATE_OUTPUT_JSON:
-    # informatie over assignments
-    assignments = instance.get_assignments()
-    assignments_json = {key: [assignment.as_dict() for assignment in value] for key, value in assignments.items()}
-    with open("assignments.json", "w") as file:
-        json.dump(assignments_json, file, indent=4)
-
-    # informatie over leerlingen zonder school
-    not_assigned = instance.filter_kinderen_not_assigned()
-    not_assigned_json = {kind.id: kind.is_assigned for kind in not_assigned.values()}
-    with open("not_assigned.json", "w") as file:
-        json.dump(not_assigned_json, file, indent=4)
-
-    # informatie over de scholen
-    school_info = instance.get_school_info()
-    with open("school_info.json", "w") as file:
-        json.dump(school_info, file, indent=4)
